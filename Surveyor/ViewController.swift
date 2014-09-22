d73d8b3e7dcffdd60127a3d3a300a40e9d6f45c5
Louis Cheung <lcheung.cse.127@gmail.com>
2014-09-22 02:16:59 -0400

    Add view that shows what the camera is currently capturing

--- Surveyor/ViewController.swift
+++ Surveyor/ViewController.swift
@@ -6,17 +6,49 @@
 //  Copyright (c) 2014 Louis Cheung. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cameraView: UIView!
+    
+    var captureSession: AVCaptureSession!
+    var cameraPreview: AVCaptureVideoPreviewLayer!
+
+// --------------------------------------------------------------------------------------------------- MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Create capture session.
+        self.captureSession  = AVCaptureSession()
+        self.captureSession.sessionPreset = AVCaptureSessionPresetHigh
+
+        // Add the rear camera as the input device to the capture session.
+        var error: NSError?
+        let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        let deviceInput = AVCaptureDeviceInput.deviceInputWithDevice(device, error: &error) as AVCaptureDeviceInput
+
+        if let unwrappedObject = error {
+            NSLog("<%@:%d> %@", __FILE__.lastPathComponent, __LINE__, unwrappedObject)
+        }
+        else {
+            self.captureSession.addInput(deviceInput)
+        }
+
+        // Create the preview layer and add it to the view that displays the camera image.
+        self.cameraPreview = AVCaptureVideoPreviewLayer(session: self.captureSession)
+        self.cameraView.layer.addSublayer(self.cameraPreview)
+
+        // Start the camera.
+        self.captureSession.startRunning()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidLayoutSubviews() {
+        // Configure the preview layer.
+        self.cameraPreview.videoGravity = AVLayerVideoGravityResizeAspectFill
+        self.cameraPreview.frame = CGRect(x: 0, y: 0, width: self.cameraView.frame.size.width,
+            height: self.cameraView.frame.size.height)
     }
 }
