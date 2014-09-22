bea57d28cd1a071157b7d8943b1f9fd73d0d2926
Louis Cheung <lcheung.cse.127@gmail.com>
2014-09-21 21:48:39 -0400

    Initial Commit

---
+++ Surveyor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Surveyor
+//
+//  Created by Louis Cheung on 9/21/14.
+//  Copyright (c) 2014 Louis Cheung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
